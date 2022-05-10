edd4a488cbc69187c05204f1bc42b9a085dff07c
Tom Diggelmann <mail@tomdgm.ch>
2022-05-10 16:15:07 +0200

    update entrypoint.sh to start correct db service

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "starting services ..."
 
 service ssh start
-service mysql start
+service mariadb start
 APACHE_RUN_USER=competitor APACHE_RUN_GROUP=competitor apache2-foreground
 
 echo "...done!"
